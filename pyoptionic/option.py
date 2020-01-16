c8936f7b8817b342fdf3cc827fa92c978b164368
seowjiajun <seowjiajun@gmail.com>
2020-01-16 22:29:00 +0800

    Modify option.py and strategies.py

--- pyoptionic/option.py
+++ pyoptionic/option.py
@@ -5,18 +5,53 @@ from matplotlib import style
 
 # Class for a naked option
 class Option(object):
-    def __init__(self, option_type, premium, strike_price):
+    def __init__(self, position, option_type, premium, strike_price):
         option_types = ('call', 'put')
         if option_type not in option_types:
             raise('Option type can only be call or put')
+        if position not in ('long', 'short'):
+            raise('Position is not specified')
         self.option_type = option_type
         self.premium = premium
         self.strike_price = strike_price
-        # Breakeven Point (BEP)
-        self.bep = strike_price + premium if option_type == 'call' else strike_price - premium
-        # Plot settings
-        self.x_extend = 100
+        self.position = position
 
+    def __str__(self):
+        return '{} {}'.format(self.position, self.option_type)
+
+    def __repr__(self):
+        return 'Option("{}", "{}", {}, {})'.format(
+            self.position,
+            self.option_type,
+            self.premium,
+            self.strike_price)
+
+    # Breakeven Point (BEP)
+    def bep(self):
+        if self.option_type == 'call':
+            breakeven = self.strike_price + self.premium
+        elif self.option_type == 'put':
+            breakeven = self.strike_price - self.premium
+
+        return breakeven
+
+    def max_gain(self):
+        if self.position == 'long':
+            gain = 'unlimited'
+        elif self.position == 'short':
+            gain = self.premium
+
+        return gain
+
+    def max_loss(self):
+        if self.position == 'long':
+            loss = self.premium
+        elif self.position == 'short':
+            loss = 'unlimited'
+
+        return loss
+
+    # Intrinsic Value
     def in_val(self, stock_price):
         if self.option_type == 'call':
             if stock_price > self.strike_price:
@@ -31,82 +66,288 @@ class Option(object):
 
         return intrinsic_val
 
+    # Time Value
+    def time_val(self, stock_price):
+        return self.premium - self.in_val(stock_price)
+
+    # ITM, ATM or OTM
     def moneyness(self, stock_price):
-        if self.option_type == 'call':
+        if stock_price == self.strike_price:
+            money = 'ATM'
+        # Long Call
+        elif self.position == 'long' and self.option_type == 'call':
             if stock_price > self.strike_price:
                 money = 'ITM'
-            elif stock_price == self.strike_price:
-                money = 'ATM'
-            else:
+            elif stock_price < self.strike_price:
                 money = 'OTM'
-        elif self.option_type == 'put':
+        # Long Put
+        elif self.position == 'long' and self.option_type == 'put':
             if stock_price < self.strike_price:
                 money = 'ITM'
-            elif stock_price == self.strike_price:
-                money = 'ATM'
-            else:
+            elif stock_price > self.strike_price:
+                money = 'OTM'
+        # Short Call
+        elif self.position == 'short' and self.option_type == 'call':
+            if stock_price < self.strike_price:
+                money = 'ITM'
+            elif stock_price > self.strike_price:
+                money = 'OTM'
+        # Short Put
+        elif self.position == 'short' and self.option_type == 'put':
+            if stock_price > self.strike_price:
+                money = 'ITM'
+            elif stock_price < self.strike_price:
                 money = 'OTM'
 
         return money
 
-    def plot_pnl(self):
+    # Y-intercept of Profit & Loss Diagram
+    def intercept(self, position, option_type):
+        premium = self.premium
+        strike_price = self.strike_price
+
+        if position == 'long' and option_type == 'call':
+            c = -premium - strike_price
+        elif position == 'short' and option_type == 'call':
+            c = premium + strike_price
+        elif position == 'long' and option_type == 'put':
+            c = strike_price - premium
+        elif position == 'short' and option_type == 'put':
+            c = premium - strike_price
+
+        return c
+
+    def pnl(self, x):
+        position = self.position
+        option_type = self.option_type
+        premium = self.premium
+        strike_price = self.strike_price
+        c = self.intercept(position, option_type)
+
+        if position == 'long' and option_type == 'call':
+            y = np.piecewise(
+                x,
+                [x <= strike_price, x > strike_price],
+                [-premium, lambda x: x + c])
+        elif position == 'short' and option_type == 'call':
+            y = np.piecewise(
+                x,
+                [x <= strike_price, x > strike_price],
+                [-premium, lambda x: x + c])
+        elif position == 'long' and option_type == 'put':
+            y = np.piecewise(
+                x,
+                [x <= strike_price, x > strike_price],
+                [-premium, lambda x: x + c])
+        elif position == 'short' and option_type == 'put':
+            y = np.piecewise(
+                x,
+                [x <= strike_price, x > strike_price],
+                [-premium, lambda x: x + c])
+
+        return y
+
+    # Plot Profit & Loss Diagram
+    def plot_pnl(self, x_extend=100):
         # Plot settings
         style.use('ggplot')
         plt.axhline(0, color='black')
         plt.axvline(0, color='black')
         plt.xlabel('Stock Price at Expiration')
         plt.ylabel('Profit or Loss')
-        plt.xlim(-5, self.strike_price+self.x_extend)
+        plt.xlim(-5, self.strike_price+x_extend)
 
-        if self.option_type == 'call':
-            intercept = -self.premium - self.strike_price
-            x = np.linspace(self.strike_price, self.strike_price+self.x_extend)
-            y = x + intercept
+        c = self.intercept(self.position, self.option_type)
 
-            plt.hlines(y=-self.premium, xmin=0, xmax=self.strike_price, color='r', linestyle='-')
-            plt.vlines(x=self.strike_price, ymin=-self.premium, ymax=0, color='#1949cf', label='ATM', linestyle='--')
-            plt.axvspan(0, self.strike_price, alpha=0.25, color='#d61e40', label='OTM')
-            plt.axvspan(self.strike_price, self.strike_price+self.x_extend, alpha=0.25, color='#18c918', label='ITM')
-            plt.title('Call Option Profit & Loss Diagram')
-        elif self.option_type == 'put':
-            intercept = -self.premium + self.strike_price
+        # Long Call
+        if self.position == 'long' and self.option_type == 'call':
+            x = np.linspace(self.strike_price, self.strike_price+x_extend)
+            y = x + c
+
+            plt.hlines(
+                y=-self.premium,
+                xmin=0,
+                xmax=self.strike_price,
+                color='r',
+                linestyle='-')
+            plt.vlines(
+                x=self.strike_price,
+                ymin=-self.premium,
+                ymax=0,
+                color='#1949cf',
+                label='ATM',
+                linestyle='--')
+
+            plt.axvspan(
+                0,
+                self.strike_price,
+                alpha=0.25,
+                color='#d61e40',
+                label='OTM')
+            plt.axvspan(
+                self.strike_price,
+                self.strike_price+x_extend,
+                alpha=0.25,
+                color='#18c918',
+                label='ITM')
+
+            plt.title('Long Call Profit & Loss Diagram')
+        # Short Call
+        elif self.position == 'short' and self.option_type == 'call':
+            x = np.linspace(self.strike_price, self.strike_price+x_extend)
+            y = -x + c
+
+            plt.hlines(
+                y=self.premium,
+                xmin=0,
+                xmax=self.strike_price,
+                color='r',
+                linestyle='-')
+            plt.vlines(
+                x=self.strike_price,
+                ymin=0,
+                ymax=self.premium,
+                color='#1949cf',
+                label='ATM',
+                linestyle='--')
+
+            plt.axvspan(
+                0,
+                self.strike_price,
+                alpha=0.25,
+                color='#18c918',
+                label='ITM')
+            plt.axvspan(
+                self.strike_price,
+                self.strike_price+x_extend,
+                alpha=0.25,
+                color='#d61e40',
+                label='OTM')
+
+            plt.title('Short Call Profit & Loss Diagram')
+        # Long Put
+        elif self.position == 'long' and self.option_type == 'put':
             x = np.linspace(0, self.strike_price)
-            y = -x + intercept
+            y = -x + c
 
-            plt.hlines(y=-self.premium, xmin=self.strike_price, xmax=self.strike_price+self.x_extend , color='r', linestyle='-')
-            plt.vlines(x=self.strike_price, ymin=-self.premium, ymax=0, color='#1949cf', label='ATM', linestyle='--')
-            plt.axvspan(0, self.strike_price, alpha=0.25, color='#18c918', label='ITM')
-            plt.axvspan(self.strike_price, self.strike_price+self.x_extend, alpha=0.25, color='#d61e40', label='OTM')
-            plt.title('Put Option Profit & Loss Diagram')
+            plt.hlines(
+                y=-self.premium,
+                xmin=self.strike_price,
+                xmax=self.strike_price+x_extend,
+                color='r',
+                linestyle='-')
+            plt.vlines(
+                x=self.strike_price,
+                ymin=-self.premium,
+                ymax=0,
+                color='#1949cf',
+                label='ATM',
+                linestyle='--')
+
+            plt.axvspan(
+                0,
+                self.strike_price,
+                alpha=0.25,
+                color='#18c918',
+                label='ITM')
+            plt.axvspan(
+                self.strike_price,
+                self.strike_price+x_extend,
+                alpha=0.25,
+                color='#d61e40',
+                label='OTM')
+            plt.title('Long Put Profit & Loss Diagram')
+        # Short Put
+        elif self.position == 'short' and self.option_type == 'put':
+            x = np.linspace(0, self.strike_price)
+            y = x + c
+
+            plt.hlines(
+                y=self.premium,
+                xmin=self.strike_price,
+                xmax=self.strike_price+x_extend,
+                color='r',
+                linestyle='-')
+            plt.vlines(
+                x=self.strike_price,
+                ymin=0,
+                ymax=self.premium,
+                color='#1949cf',
+                label='ATM',
+                linestyle='--')
+
+            plt.axvspan(
+                0,
+                self.strike_price,
+                alpha=0.25,
+                color='#d61e40',
+                label='OTM')
+            plt.axvspan(
+                self.strike_price,
+                self.strike_price+x_extend,
+                alpha=0.25,
+                color='#18c918',
+                label='ITM')
+            plt.title('Short Put Profit & Loss Diagram')
 
         plt.plot(x, y, '-r')
         plt.legend()
         plt.show()
 
-    def plot_inval(self):
+    def plot_inval(self, x_extend=100):
         # Plot settings
         style.use('ggplot')
         plt.axhline(0, color='black')
         plt.axvline(0, color='black')
         plt.xlabel('Stock Price at Expiration')
         plt.ylabel('Intrinsic Value of Option')
-        plt.xlim(-5, self.strike_price+self.x_extend)
+        plt.xlim(-5, self.strike_price+x_extend)
 
         if self.option_type == 'call':
-            x = np.linspace(self.strike_price, self.strike_price+self.x_extend)
+            x = np.linspace(self.strike_price, self.strike_price+x_extend)
             y = x + -self.strike_price
 
-            plt.hlines(y=0, xmin=0, xmax=self.strike_price, color='r', linestyle='-')
-            plt.axvspan(0, self.strike_price, alpha=0.25, color='#d61e40', label='OTM')
-            plt.axvspan(self.strike_price, self.strike_price+self.x_extend, alpha=0.25, color='#18c918', label='ITM')
+            plt.hlines(
+                y=0,
+                xmin=0,
+                xmax=self.strike_price,
+                color='r',
+                linestyle='-')
+            plt.axvspan(
+                0,
+                self.strike_price,
+                alpha=0.25,
+                color='#d61e40',
+                label='OTM')
+            plt.axvspan(
+                self.strike_price,
+                self.strike_price+x_extend,
+                alpha=0.25,
+                color='#18c918',
+                label='ITM')
             plt.title('Call Option Intrinsic Value Diagram')
         elif self.option_type == 'put':
             x = np.linspace(0, self.strike_price)
             y = -x + self.strike_price
 
-            plt.hlines(y=-0, xmin=self.strike_price, xmax=self.strike_price+self.x_extend , color='r', linestyle='-')
-            plt.axvspan(0, self.strike_price, alpha=0.25, color='#18c918', label='ITM')
-            plt.axvspan(self.strike_price, self.strike_price+self.x_extend, alpha=0.25, color='#d61e40', label='OTM')
+            plt.hlines(
+                y=-0,
+                xmin=self.strike_price,
+                xmax=self.strike_price+x_extend,
+                color='r',
+                linestyle='-')
+            plt.axvspan(
+                0,
+                self.strike_price,
+                alpha=0.25,
+                color='#18c918',
+                label='ITM')
+            plt.axvspan(
+                self.strike_price,
+                self.strike_price+x_extend,
+                alpha=0.25,
+                color='#d61e40',
+                label='OTM')
             plt.title('Put Option Intrinsic Value Diagram')
 
         plt.plot(x, y, '-r')
--- pyoptionic/strategies.py
+++ pyoptionic/strategies.py
@@ -1,15 +1,247 @@
-from option import Options
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
 
-class Strategies(object):
-    # requires both options to have the same strike price
-    @classmethod
-    def long_straddle(cls, call_option, put_option, plot=True):
+
+class LongStradle(object):
+    def __init__(self, long_call, long_put):
+        if long_call.strike_price != long_put.strike_price:
+            raise('Strike prices must be equivalent')
+        self.long_call = long_call
+        self.long_put = long_put
+        self.strike_price = long_call.strike_price
+        self.premium = long_call.premium + long_put.premium
+
+    def __str__(self):
+        return 'Long Stradle'
+
+    def __repr__(self):
+        return 'LongStradle({}, {})'.format(self.long_call, self.long_put)
+
+    def bep(self):
         pass
-    
-    @classmethod
-    def iron_condor(cls):
+
+    def in_val(self, stock_price):
+        if stock_price < self.strike_price:
+            intrinsic_val = self.strike_price - stock_price
+        elif stock_price == self.strike_price:
+            intrinsic_val = 0
+        else:
+            intrinsic_val = stock_price - self.strike_price
+
+        return intrinsic_val
+
+    def moneyness(self, stock_price):
+        if stock_price < self.strike_price:
+            money = 'ITM'
+        elif stock_price == self.strike_price:
+            money = 'ATM'
+        elif stock_price > self.strike_price:
+            money = 'ITM'
+
+        return money
+
+    def plot_pnl(self, x_extend=100):
         pass
-    
-    @classmethod
-    def iron_butterfly(cls):
+
+    def plot_inval(self, x_extend=100):
+        # Plot settings
+        style.use('ggplot')
+        plt.axhline(0, color='black')
+        plt.axvline(0, color='black')
+        plt.xlabel('Stock Price at Expiration')
+        plt.ylabel('Intrinsic Value of Long Straddle')
+        plt.xlim(-5, self.strike_price+x_extend)
+
+        x_put = np.linspace(0, self.strike_price)
+        x_call = np.linspace(self.strike_price, self.strike_price+x_extend)
+
+        y_put = -x_put + self.strike_price
+        y_call = x_call - self.strike_price
+
+        plt.plot(x_put, y_put, '-r')
+        plt.plot(x_call, y_call, 'r')
+        plt.legend()
+        plt.show()
+
+
+class BullCallSpread(object):
+    def __init__(self, long_call, short_call):
+        if short_call.strike_price <= long_call.strike_price:
+            raise('Short call strike price must be '
+                  'higher than long call strike price')
+        self.long_call = long_call
+        self.short_call = short_call
+
+    def __str__(self):
+        return 'Bull Call Spread'
+
+    def __repr__(self):
+        return 'BullCallSpread({}, {})'.format(self.long_call, self.short_call)
+
+    def spread_width(self):
+        return self.short_call.strike_price - self.long_call.strike_price
+
+    def net_debit(self):
+        return self.long_call.premium - self.short_call.premium
+
+    def max_loss(self):
+        return self.debit() * 100
+
+    def max_profit(self):
+        return (self.spread_width() - self.net_debit()) * 100
+
+    def bep(self):
+        return self.long_call.strike_price + self.net_debit()
+
+    def in_val(self):
+        pass
+
+    def moneyness(self):
+        pass
+
+
+class BullPutSpread(object):
+    def __init__(self, long_put, short_put):
+        if long_put.strike_price >= short_put.strike_price:
+            raise('Long put strike price must be '
+                  'lower than short put strike price')
+        self.long_put = long_put
+        self.short_put = short_put
+
+    def __str__(self):
+        return 'Bull Put Spread'
+
+    def __repr__(self):
+        return 'BullPutSpread({}, {})'.format(self.long_put, self.short_put)
+
+    def spread_width(self):
+        return self.short_put.strike_price - self.long_put.strike_price
+
+    def net_credit(self):
+        return self.short_put.premium - self.long_put.premium
+
+    def bep(self):
+        return self.short_put.strike_price - self.net_credit()
+
+    def max_loss(self):
+        return (self.spread_width() - self.net_credit()) * 100
+
+    def max_profit(self):
+        return self.net_credit() * 100
+
+    def in_val(self):
+        pass
+
+    def moneyness(self):
+        pass
+
+
+class BearCallSpread(object):
+    def __init__(self, long_call, short_call):
+        if long_call.strike_price <= short_call.strike_price:
+            raise('Long call strike price must be '
+                  'higher than short call strike price')
+        long_call = self.long_call
+        short_call = self.short_call
+
+    def __str__(self):
+        return 'Bear Call Spread'
+
+    def __repr__(self):
+        return 'BearCallSpread({}, {})'.format(self.long_call, self.short_call)
+
+    def spread_width(self):
+        return self.long_call.strike_price - self.short_call.strike_price
+
+    def net_credit(self):
+        return self.short_call.premium - self.long_call.premium
+
+    def bep(self):
+        return self.short_call.strike_price + self.net_credit()
+
+    def max_loss(self):
+        return (self.spread_width() - self.net_credit()) * 100
+
+    def max_gain(self):
+        return self.net_credit() * 100
+
+    def in_val(self):
+        pass
+
+    def moneyness(self):
+        pass
+
+
+class BearPutSpread(object):
+    def __init__(self, long_put, short_put):
+        if long_put.strike_price <= short_put.strike_price:
+            raise('Long put strike price must be '
+                  'higher than short put strike price')
+        self.long_put = long_put
+        self.short_put = short_put
+
+    def __str__(self):
+        return 'Bear Put Spread'
+
+    def __repr__(self):
+        return 'BearPutSpread({}, {})'.format(self.long_put, self.short_put)
+
+    def spread_width(self):
+        return self.long_put.strike_price - self.short_put.strike_price
+
+    def net_debit(self):
+        return self.long_put.premium - self.short_put.premium
+
+    def bep(self):
+        return self.long_put.strike_price - self.net_debit()
+
+    def max_loss(self):
+        return self.net_debit() * 100
+
+    def max_gain(self):
+        return (self.spread_width() - self.net_debit()) * 100
+
+    def in_val(self):
+        pass
+
+    def moneyness(self):
+        pass
+
+
+class IronCondor(object):
+    def __init__(self, long_call, long_put, short_call, short_put):
+        self.long_call = long_call
+        self.long_put = long_put
+        self.short_call = short_call
+        self.short_put = short_put
+
+    def __str__(self):
+        return 'Iron Condor'
+
+    def __repr__(self):
+        return 'IronCondor({}, {}, {}, {})'.format(
+            self.long_call, self.long_put, self.short_call, self.short_put)
+
+    def in_val(self):
+        pass
+
+    def moneyness(self):
+        pass
+
+
+class IronButterfly(object):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return 'Iron Butterfly'
+
+    def __repr__(self):
+        return 'IronButterfly()'.format()
+
+    def in_val(self):
+        pass
+
+    def moneyness(self):
         pass
